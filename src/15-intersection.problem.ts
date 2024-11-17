fd19f6d3ca1dfab8954eb5bf9bd57576d7554279
Absoluteloser <motkulwarbalaji805@gmail.com>
2024-11-17 05:35:26 +0530

    used Array and Post[] and &

--- src/15-intersection.problem.ts
+++ src/15-intersection.problem.ts
@@ -3,7 +3,7 @@ interface User {
   firstName: string;
   lastName: string;
   // posts: Post[];
-  posts: Array<Post>;
+  // posts: Array<Post>;
 }
 
 interface Post {
@@ -16,7 +16,7 @@ interface Post {
  * How do we type this return statement so it's both
  * User AND { posts: Post[] }
  */
-export const getDefaultUserAndPosts = (): User => {
+export const getDefaultUserAndPosts = (): User & { posts: Post[] } => {
   return {
     id: "1",
     firstName: "Matt",
